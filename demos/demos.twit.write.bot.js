f07b249951e07c37a0f6b508ad602933cd9859f8
Ganesh B <ganeshsurfs@gmail.com>
2024-03-14 19:07:50 +0530

    [COMMT] First twitter-post-bot package commit

--- demos/demos.twit.write.bot.js
+++ demos/demos.twit.write.bot.js
@@ -1,11 +1,11 @@
 /**
  * 
- * Package: 
+ * Package: twitter-post-bot
  * Author: Ganesh B
  * Description: 
- * Install: npm i  --save
+ * Install: npm i twitter-post-bot --save
  * Github: https://github.com/ganeshkbhat/
- * npmjs Link: https://www.npmjs.com/package/
+ * npmjs Link: https://www.npmjs.com/package/twitter-post-bot
  * File: index.js
  * File Description: 
  * 
