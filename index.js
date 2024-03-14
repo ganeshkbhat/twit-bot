711a9359bd1f6e3c7f56013da6c0032b5d964845
Ganesh B <ganeshsurfs@gmail.com>
2024-03-14 18:54:01 +0530

    [COMMT] First twitter-post-bot package commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: index.js
+ * File Description: https://www.npmjs.com/package/twit
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
